aab81f1e9aee9e0e4bc57337d7bece5c6211e449
Rahul Chauhan <omrahulchauhan@gmail.com>
2021-04-24 15:54:15 +0530

    Formated older code

--- threading/odd_even_sum.cpp
+++ threading/odd_even_sum.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
-#include<chrono>
-#include<thread>
-#include<climits>
+#include <chrono>
+#include <thread>
+#include <climits>
 using namespace std;
 
 typedef unsigned long long ull;
 
-void evenSum(int from, int to, ull *evenResult){
-	*evenResult =0;
-	for(int i=from; i<=to; i++)
-		if(!(i&1)) *evenResult += i;
+void evenSum(int from, int to, ull *evenResult)
+{
+	*evenResult = 0;
+	for (int i = from; i <= to; i++)
+		if (!(i & 1))
+			*evenResult += i;
 }
 
-void oddSum(int from, int to, ull *oddResult){
-	*oddResult=0;
-	for(int i=from; i<= to; i++)
-		if(i&1) *oddResult += i;
+void oddSum(int from, int to, ull *oddResult)
+{
+	*oddResult = 0;
+	for (int i = from; i <= to; i++)
+		if (i & 1)
+			*oddResult += i;
 }
 
-int main(){
+int main()
+{
 
-	int from=0, to=999999999;
-	ull evenResult=0, oddResult=0;
-	cout<<INT_MAX<<endl;
+	int from = 0, to = 999999999;
+	ull evenResult = 0, oddResult = 0;
+	cout << INT_MAX << endl;
 	auto startTime = std::chrono::high_resolution_clock::now();
 	std::thread t1(evenSum, from, to, (&evenResult));
 	std::thread t2(oddSum, from, to, (&oddResult));
@@ -30,19 +35,19 @@ int main(){
 	t2.join();
 	auto endTime = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
-	cout<<"Threading Results : "<<endl;
-	cout<<"Odd Sum = "<<oddResult<<endl;
-	cout<<"Even Sum = "<<evenResult<<endl;
-	cout << "Sec: " << duration.count()/1000000 << endl<<endl;
+	cout << "Threading Results : " << endl;
+	cout << "Odd Sum = " << oddResult << endl;
+	cout << "Even Sum = " << evenResult << endl;
+	cout << "Sec: " << duration.count() / 1000000 << endl
+		 << endl;
 
- 	startTime = std::chrono::high_resolution_clock::now();
+	startTime = std::chrono::high_resolution_clock::now();
 	evenSum(from, to, &evenResult);
 	oddSum(from, to, &oddResult);
 	endTime = std::chrono::high_resolution_clock::now();
 	duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
-	cout<<"Non threaded (Single Main Thread) results : "<<endl;
-	cout<<"Odd Sum = "<<oddResult<<endl;
-	cout<<"Even Sum = "<<evenResult<<endl;
-	cout << "Sec: " << duration.count()/1000000 << endl;
-
-	}
+	cout << "Non threaded (Single Main Thread) results : " << endl;
+	cout << "Odd Sum = " << oddResult << endl;
+	cout << "Even Sum = " << evenResult << endl;
+	cout << "Sec: " << duration.count() / 1000000 << endl;
+}
--- threading/thread_management.cpp
+++ threading/thread_management.cpp
@@ -1,28 +1,32 @@
-#include<iostream>
-#include<thread>
+#include <iostream>
+#include <thread>
 
 using namespace std;
 
-void func(){
-    cout<<"T1:: Hello World!!\n";
+void func()
+{
+    cout << "T1:: Hello World!!\n";
 }
 
-class functor {
-    public:
-        void operator()(){
-            cout<<"Thread:: Hello from functor"<<endl;
-        }
+class functor
+{
+public:
+    void operator()()
+    {
+        cout << "Thread:: Hello from functor" << endl;
+    }
 };
 
-int main(){
+int main()
+{
     thread t1(func);
-    cout<<"Main :: Hello World!\n";
-    cout<<"Main::ThreadId =   "<<this_thread::get_id()<<endl;
-    cout<<"T1::ThreadId =  "<<t1.get_id()<<endl;
+    cout << "Main :: Hello World!\n";
+    cout << "Main::ThreadId =   " << this_thread::get_id() << endl;
+    cout << "T1::ThreadId =  " << t1.get_id() << endl;
 
     //thread t2 = t1; Not allowed since Threads cannot be copied
-    //thread t3(functor()); Not allowed since it matches a function declaration, 
-    cout<<"Number of thread contexts =  "<<thread::hardware_concurrency()<<endl; 
+    //thread t3(functor()); Not allowed since it matches a function declaration,
+    cout << "Number of thread contexts =  " << thread::hardware_concurrency() << endl;
 
     thread t3((functor()));
     t3.join();
@@ -31,15 +35,14 @@ int main(){
     // t1.detach();
     try
     {
-    // int i=1000000; while(i--);
-    cout<<"Main:: Hello again World!\n";
+        // int i=1000000; while(i--);
+        cout << "Main:: Hello again World!\n";
     }
-    catch(...)
+    catch (...)
     {
         t2.join();
         throw;
     }
-    if (t2.joinable()) t2.join();
-
-    
+    if (t2.joinable())
+        t2.join();
 }
