80d035b4d06c55bafd9afaf4a2459c7a08330adc
Rahul Chauhan <omrahulchauhan@gmail.com>
2021-04-24 15:53:10 +0530

    Write to a file

--- file_management/basic_in_out.cpp
+++ file_management/basic_in_out.cpp
@@ -14,5 +14,6 @@ int main()
     //Lets write to a file
     std::ofstream outFile;          //1. We create an output file stream object for this purpose
     outFile.open("output.txt");     //2. We open a connection to a file on the storage system
-    outFile << "This is a outfile"; //3.Write to the file
+    outFile << "This is a outfile"; //3. Write to the file
+    outFile.close();                //4. We close the file, so that the lock can be released and other parts of the program can access the file
 }
